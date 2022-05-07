0a3c49ff9b5e872a7078d26f486766d922c38f27
Senty <haydenbugatti@gmail.com>
2022-05-06 17:33:27 -0700

    keypad actually lets you enter numbers now

--- serverManager.lua
+++ serverManager.lua
@@ -18,14 +18,19 @@ hosting = false
 connected = false
 
 tick = 0
+userServerCode = ""
 
 function onTick()
     LifeBoatAPI.LBTouchScreen:lbtouchscreen_onTick() -- touchscreen handler provided by LifeBoatAPI. Handles checking for clicks/releases etc.
-    inputX = input.getNumber(3)
     inputY = input.getNumber(4)
+    inputX = input.getNumber(3)
     ppressed = input.getBool(1) and not isPressed
     isPressed = input.getBool(1)
     draw = input.getBool(2)
+
+    if not draw then
+        tick = 0
+    end
 
     serverCode = property.getNumber("Server code")
     if serverCode == 0 then
@@ -52,8 +57,7 @@ function onTick()
         output.setNumber(1, tonumber(serverCode))
     else
         output.setBool(1, false)
-        output.setNumber(1,0)
-    end
+     end
 end
 
 function onDraw()
@@ -64,7 +68,7 @@ function onDraw()
         --if not connected then should probably prompt to connect or host
         if not connected and not hosting then
             screen.setColor(255,255,255)
-            screen.drawText(1,1, "Enter Server Code: ")
+            screen.drawText(1,1, "Enter Server Code: "..userServerCode)
             screen.drawText(1,15, "Or start hosting: ")
             hostButton:lbstyledbutton_draw()
             drawKeypad(138,lerp(96,63,tick),true)
@@ -79,24 +83,24 @@ end
 
 function drawKeypad(x,y,outline)
     local table = {"1","2","3","4","5","6","7","8","9","d","0","e"}
-    butt(x,y,table[1],{255,255,255},outline)
-    butt(x+7,y,table[2],{255,255,255}, outline)
-    butt(x+14,y,table[3],{255,255,255}, outline)
-    butt(x,y+8,table[4],{255,255,255}, outline)
-    butt(x+7,y+8,table[5],{255,255,255}, outline)
-    butt(x+14,y+8,table[6],{255,255,255}, outline)
-    butt(x,y+16,table[7],{255,255,255}, outline)
-    butt(x+7,y+16,table[8],{255,255,255}, outline)
-    butt(x+14,y+16,table[9],{255,255,255}, outline)
-    butt(x,y+24,table[10],{255,0,0}, outline)
-    butt(x+7,y+24,table[11],{255,255,255}, outline)
-    butt(x+14,y+24,table[12],{0,255,0}, outline)
+    if butt(x,y,table[1],{255,255,255},outline) then userServerCode = userServerCode .. "1" end
+    if butt(x+7,y,table[2],{255,255,255}, outline) then userServerCode = userServerCode .. "2" end
+    if butt(x+14,y,table[3],{255,255,255}, outline) then userServerCode = userServerCode .. "3" end
+    if butt(x,y+8,table[4],{255,255,255}, outline) then userServerCode = userServerCode .. "4" end
+    if butt(x+7,y+8,table[5],{255,255,255}, outline) then userServerCode = userServerCode .. "5" end
+    if butt(x+14,y+8,table[6],{255,255,255}, outline) then userServerCode = userServerCode .. "6" end
+    if butt(x,y+16,table[7],{255,255,255}, outline) then userServerCode = userServerCode .. "7" end
+    if butt(x+7,y+16,table[8],{255,255,255}, outline) then userServerCode = userServerCode .. "8" end
+    if butt(x+14,y+16,table[9],{255,255,255}, outline) then userServerCode = userServerCode .. "9" end
+    if butt(x,y+24,table[10],{255,0,0}, outline) then userServerCode = string.sub(userServerCode, 0, string.len(userServerCode)-1) end
+    if butt(x+7,y+24,table[11],{255,255,255}, outline) then userServerCode = userServerCode .. "0" end
+    if butt(x+14,y+24,table[12],{0,255,0}, outline) then userServerCode = userServerCode .. "e" end
 end
 
 function butt(x,y,l,color,outline) --bottom left, letter, color (rgb table)
     local sc = screen
     sc.setColor(0,0,0)
-    local fuck = isPressed and isPointInRectangle(inputX, inputY, x+1, y+1, 6, 7)
+    local fuck = ppressed and isPointInRectangle(inputX, inputY, x+1, y+1, 6, 7)
     if fuck then sc.setColor(20,20,20) sc.drawRectF(x+1,y+1, 6, 7) else sc.drawRectF(x,y, 7, 8) end
     if outline then sc.setColor(255,255,255) sc.drawRect(x,y, 7, 8) end
     sc.setColor(color[1],color[2],color[3])
