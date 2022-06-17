b8937f81628edb41bd572c3bfbd91661a0409b16
Senty <haydenbugatti@gmail.com>
2022-06-17 01:48:34 -0700

    lol if i know

--- chatManager.lua
+++ chatManager.lua
@@ -1,1 +1,8 @@
+-- Author: SentyFunBall
+-- GitHub: https://github.com/SentyFunBall
+-- Workshop: <WorkshopLink>
+
+--Code by STCorp. Do not reuse.--
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
 --chatmanager
